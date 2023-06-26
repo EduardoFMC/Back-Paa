4824086fbc45a3d70d343c0aa4cad08a593333ca
Mayara Chew Marinho <mayarachew.21@gmail.com>
2023-06-26 02:13:40 -0300

    Add basic model to post method

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import pipeline
 
 app = FastAPI()
 
@@ -19,7 +20,12 @@ class Data(BaseModel):
 
 @app.post("/")
 async def process_string(data: Data):
-    # Modify the input string
-    output_string = data.question + " (modified)"
+    with open('context.txt') as f:
+        context = f.read()
+
+    tokenizer = AutoTokenizer.from_pretrained("deepset/bert-base-uncased-squad2")
+    model = AutoModelForQuestionAnswering.from_pretrained("deepset/bert-base-uncased-squad2")
+    question_answerer = pipeline("question-answering", model=model, tokenizer=tokenizer)
+    output_string = question_answerer(question=data.question, context=context)['answer']
 
     return {"output_string": output_string}
